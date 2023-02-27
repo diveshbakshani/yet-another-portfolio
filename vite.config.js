f420d71756d1aeabe9d69274044975bb7d593343
diveshbakshani <divebakshani@gmail.com>
2023-02-26 23:22:56 -0500

    is it working yet

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/yet-another-portfolio/",
 });
-=======
-  base: '/yet-another-portfolio/',
-})
->>>>>>> main
